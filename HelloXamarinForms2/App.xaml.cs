77170beee948e70427821b01a0abf85f338ebbdc
Jonathan Bachelor <jon_bachelor@me.com>
2017-06-04 14:24:42 -0700

    Initial commit:  Fully updated & functional Prism-Unity Xamarin.Forms app.

---
+++ HelloXamarinForms2/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Unity;
+using HelloXamarinForms2.Views;
+
+namespace HelloXamarinForms2
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync(nameof(MainPage));
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+		}
+	}
+}
+
---
+++ HelloXamarinForms2/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace HelloXamarinForms2.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
