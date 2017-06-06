ca29473f223802c8fd257ccf7c888dfbb3574649
Jonathan Bachelor <jon_bachelor@me.com>
2017-06-05 21:01:23 -0700

    Changing tipPercentage to an int rather than a double to make calculations easier. Added TipAmount property, which will be re-calculated whenever either TipPercentage or BillAmount are changed.

--- HelloXamarinForms2/ViewModels/TipCalculatorPageViewModel.cs
+++ HelloXamarinForms2/ViewModels/TipCalculatorPageViewModel.cs
@@ -3,28 +3,51 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 namespace HelloXamarinForms2.ViewModels
 {
 	public class TipCalculatorPageViewModel : BindableBase
 	{
-		private double _tipPercentage;
-		public double TipPercentage
+		private int _tipPercentage;
+		public int TipPercentage
 		{
 			get { return _tipPercentage; }
-			set { SetProperty(ref _tipPercentage, value); }
+			set
+			{
+				Debug.WriteLine($"**** {this.GetType().Name}.{nameof(TipPercentage)}:  Setting tip percent to {value}");
+				SetProperty(ref _tipPercentage, value);
+				CalculateTip();
+			}
 		}
 
 		private double _billAmount;
 		public double BillAmount
 		{
 			get { return _billAmount; }
-			set { SetProperty(ref _billAmount, value); }
+			set
+			{
+				SetProperty(ref _billAmount, value);
+				CalculateTip();
+			}
+		}
+
+		private double _tipAmount;
+		public double TipAmount
+		{
+			get { return _tipAmount; }
+			set { SetProperty(ref _tipAmount, value); }
 		}
 
 		public TipCalculatorPageViewModel()
 		{
-			TipPercentage = 0.18;
+			TipPercentage = 18;
+		}
+
+		private void CalculateTip()
+		{
+			TipAmount = (BillAmount * TipPercentage) / 100.0;
+			Debug.WriteLine($"**** {this.GetType().Name}.{nameof(CalculateTip)}:  TipPercentage={TipPercentage}, BillAmount={BillAmount}, TipAmount={TipAmount}");
 		}
 	}
 }
